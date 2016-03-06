4fb3e232af1d40decf2676c47f570f84534d7bb5
beimarhuarachi <beimar020@gmail.com>
2016-03-06 19:58:37 -0400

    deleted integrity attribute on editor.php and index.php files

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>CODE</title>
-		<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
 	</head>
 	<body>
 		<h1 class="text-center">Hello World</h1>
 
 
 		<script src="js/jquery-2.2.1.min.js"></script>
-		<script src="js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
- 		<script src="Hello World"></script>
+		<script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
 	</body>
 </html>
