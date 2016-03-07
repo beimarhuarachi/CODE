568ef8f0733bd0444df76fccb66f47871773b412
beimarhuarachi <beimar020@gmail.com>
2016-03-06 23:24:50 -0400

    task and type entities implemented in sql file and database conection class implemented in php

--- vista/asignacion_tareas.php
+++ vista/asignacion_tareas.php
@@ -8,32 +8,77 @@
 
         <!-- ESTILOS DE BOOTSTRAP -->
         <link rel="stylesheet" href="../css/bootstrap.min.css" crossorigin="anonymous">
-                <link href="../css/stylesmenu.css" rel="stylesheet">
+        <link href="../css/stylesmenu.css" rel="stylesheet">
 
     </head>
     <body>
         <?php require_once 'inc/menu_navegacion.php'; ?>
 
         <div class="container">
-
-        <div class="row">
-            <div class="page-header">
-              <h1>CODE<small>  Asignar tarea</small></h1>
-            </div>
-            
-        </div>
-          
-          <div class="row">
-              <h3>Lista de tareas</h3>
-              <div class="list-group">
-                <button type="button" class="list-group-item">Cras justo odio</button>
-                <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
-                <button type="button" class="list-group-item">Morbi leo risus</button>
+                
+              <div class="row">
+                <div class="page-header">
+                  <h1>CODE<small>  Asignar tarea</small></h1>
               </div>
-          </div>
-          
+
+              </div>
+                
+              <div class="row">
+                  <div class="panel panel-primary">
+                      <div class="panel-heading">
+                          <h3 class="panel-title">Nueva tarea</h3>
+                      </div>
+                      <div class="panel-body">
+                          <form action="" method="POST" role="form">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label for="">Descripcion</label>
+                                      <textarea name="" id="input" class="form-control" rows="3" required="required"></textarea>
+                                  </div>
+                                  
+                                  <div class="form-group">
+                                      <label for="">Fecha limite</label>
+                                      <input type="date" class="form-control" id="" placeholder="Input field">
+                                  </div>
+
+
+                              </div>  
+                                
+                              <div class="col-md-6">
+
+                                  <div class="form-group">
+                                      <label for="">Arbol</label>
+                                      <input type="text" class="form-control" id="" placeholder="Input field">
+                                  </div>
+
+                                  <div class="form-group">
+                                      <label for="">Tipo</label>
+                                      <select name="" id="input" class="form-control" required="required">
+                                          <option value="">primero</option>
+                                      </select>
+                                      
+                                  </div>
+                                  <button type="submit" class="btn btn-primary">Guardar</button>
+                                  
+                              </div>  
+                              
+                          
+                          </form>
+                      </div>
+                  </div>
+              </div>
+              
+              <div class="row">
+                  <h3>Lista de tareas</h3>
+                  <div class="list-group">
+                    <button type="button" class="list-group-item">Cras justo odio</button>
+                    <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
+                    <button type="button" class="list-group-item">Morbi leo risus</button>
+                </div>
+              </div>
+
         </div>
-        
+
         <!-- INCLUCION DE BOOTSTRAP (REQUIERE DE JQUERY) -->
         <script src="js/jquery-2.2.1.min.js"></script>
         <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
