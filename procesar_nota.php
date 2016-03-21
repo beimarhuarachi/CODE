d159ac92da298fe3ef94b913f11c5bc40e56972b
rod8 <rivera.rodrigo08@gmail.com>
2016-03-20 23:22:32 -0400

    Cambio forma redireccionar
    EL otro no me redireccionaba de forma correcta así que..

--- procesar_nota.php
+++ procesar_nota.php
@@ -8,5 +8,6 @@ $id_cal = $_POST['id_cal'];
 $sql = "update calificacion set nota = '".$nota."' where id=".$id_cal;
 
 mysql_query($sql);
-echo "<script> alert('kjfklasjkldfj');</script>";
-header('Location: calificar.php');
+//echo "<script> alert('kjfklasjkldfj');</script>";
+echo '<script>window.location="calificar.php";</script>';
+//header('Location: calificar.php');
